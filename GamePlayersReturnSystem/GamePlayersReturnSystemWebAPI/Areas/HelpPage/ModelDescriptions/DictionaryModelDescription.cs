b60e0855afaaf621cb9e9c166d0fc60a22e6afe7
sajiao <yijun-eleven@163.com>
2018-08-20 18:15:35 +0800

    init project

---
+++ GamePlayersReturnSystem/GamePlayersReturnSystemWebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GamePlayersReturnSystemWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ GamePlayersReturnSystem/GamePlayersReturnSystemWebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GamePlayersReturnSystemWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
